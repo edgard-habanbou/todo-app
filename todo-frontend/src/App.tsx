6c15872a5dde74bf1cd7c3eb13beccc1c1d9fb15
edgard <edgard.habanbou@hotmail.com>
2024-03-05 17:28:19 +0200

    Frontend: Added Login Page as well as General Css

--- todo-frontend/src/App.tsx
+++ todo-frontend/src/App.tsx
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
 
+import Login from "./pages/Login";
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+<Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ todo-frontend/src/pages/Login/index.tsx
@@ -0,0 +1,11 @@
+function Login() {
+  return (
+    <div>
+      <div className="flex center full-screen">
+        <h3>Login</h3>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
