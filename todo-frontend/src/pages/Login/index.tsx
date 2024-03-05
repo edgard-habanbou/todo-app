e10f4c28bb4013ee7c9455f9d8317d6e8526409b
edgard <edgard.habanbou@hotmail.com>
2024-03-05 18:28:36 +0200

    Frontend: Added Loading components

--- todo-frontend/src/pages/Login/index.tsx
+++ todo-frontend/src/pages/Login/index.tsx
@@ -1,35 +1,56 @@
+import { useState } from "react";
 import "./index.css";
+import Loading from "../../components/Loading";
 
 function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = () => {
+    setLoading(true);
+    console.log(email, password);
+  };
+
   return (
-    <div className="full-screen flex center login-page">
-      <div className="background-primary login-form">
-        <div className="flex center column full-height">
-          <form className="flex column gap center full-width">
-            <div className="flex column gap">
-              <label htmlFor="email" className="color-white">
-                Email
-              </label>
-              <input type="email" id="email" name="email" className="input" />
+    <>
+      {loading && <Loading message="Loading in..." />}
+      <div className="full-screen flex center login-page">
+        <div className="background-primary login-form">
+          <div className="flex center column full-height">
+            <div className="flex column gap center full-width">
+              <div className="flex column gap">
+                <label htmlFor="email" className="color-white">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  className="input"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="flex column gap">
+                <label htmlFor="password" className="color-white">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  className="input"
+                />
+              </div>
+              <button type="submit" className="btn" onClick={handleSubmit}>
+                Login
+              </button>
             </div>
-            <div className="flex column gap">
-              <label htmlFor="password" className="color-white">
-                Password
-              </label>
-              <input
-                type="password"
-                id="password"
-                name="password"
-                className="input"
-              />
-            </div>
-            <button type="submit" className="btn">
-              Login
-            </button>
-          </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
