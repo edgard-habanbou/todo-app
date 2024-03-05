c139bc019820d1d58eaaecee5061b094a77b7ab6
edgard <edgard.habanbou@hotmail.com>
2024-03-05 03:53:10 +0200

    Backend: Cleaned code

--- todo-backend/src/todos/todos.controller.ts
+++ todo-backend/src/todos/todos.controller.ts
@@ -10,11 +10,9 @@ export class TodosController {
   async getAllTodos(
     @Req() request: Request & { user: { userId: string } },
     @Res() response: Response,
-  ): Promise<any> {
+  ): Promise<Response> {
     try {
-      const todos = await this.todosService.getTodoByUser(
-        request.user.userId as string,
-      );
+      const todos = await this.todosService.getTodoByUser(request.user.userId);
       return response.status(200).json(todos);
     } catch (error) {
       return response.status(500).json({ message: error.message });
@@ -25,7 +23,7 @@ export class TodosController {
   async getTodoById(
     @Req() request: Request,
     @Res() response: Response,
-  ): Promise<any> {
+  ): Promise<Response> {
     try {
       const todo = await this.todosService.getTodoById(request.params.id);
       return response.status(200).json(todo);
@@ -38,14 +36,11 @@ export class TodosController {
   async createTodo(
     @Req() request: Request & { user: { userId: string } },
     @Res() response: Response,
-  ): Promise<any> {
+  ): Promise<Response> {
     try {
       const todo = await this.todosService.createTodo({
-        title: request.body.title,
-        description: request.body.description,
-        priority: request.body.priority,
-        date: request.body.date,
-        userId: request.user.userId as string,
+        ...request.body,
+        userId: request.user.userId,
       });
       return response.status(201).json(todo);
     } catch (error) {
@@ -57,33 +52,24 @@ export class TodosController {
   async updateTodo(
     @Req() request: Request & { user: { userId: string } },
     @Res() response: Response,
-  ): Promise<any> {
+  ): Promise<Response> {
     try {
       const todoId = request.params.id;
       const unUpdatedTodo = await this.todosService.getTodoById(todoId);
-      const updateFields: any = {};
 
       if (unUpdatedTodo.userId !== request.user.userId) {
         return response.status(401).json({ message: 'Unauthorized' });
       }
 
-      if (request.body.title !== undefined) {
-        updateFields.title = request.body.title;
-      }
-      if (request.body.description !== undefined) {
-        updateFields.description = request.body.description;
-      }
-      if (request.body.priority !== undefined) {
-        updateFields.priority = request.body.priority;
-      }
-      if (request.body.completed !== undefined) {
-        updateFields.completed = request.body.completed;
-      }
-      if (request.body.date !== undefined) {
-        updateFields.date = new Date(request.body.date);
-      }
-      const todo = await this.todosService.updateTodoById(todoId, updateFields);
+      const updateFields = {
+        title: request.body.title,
+        description: request.body.description,
+        priority: request.body.priority,
+        completed: request.body.completed,
+        date: request.body.date ? new Date(request.body.date) : undefined,
+      };
 
+      const todo = await this.todosService.updateTodoById(todoId, updateFields);
       return response.status(200).json(todo);
     } catch (error) {
       return response.status(500).json({ message: error.message });
@@ -94,11 +80,12 @@ export class TodosController {
   async deleteTodo(
     @Req() request: Request & { user: { userId: string } },
     @Res() response: Response,
-  ): Promise<any> {
+  ): Promise<Response> {
     try {
       const unDeletedTodo = await this.todosService.getTodoById(
         request.params.id,
       );
+
       if (unDeletedTodo.userId !== request.user.userId) {
         return response.status(401).json({ message: 'Unauthorized' });
       }
