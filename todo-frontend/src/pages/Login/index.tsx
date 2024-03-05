396502ee514efe2dd14eaa0394b6e48cba3f9e4d
edgard <edgard.habanbou@hotmail.com>
2024-03-05 18:20:28 +0200

    Frontend: Added style for Login Page

--- todo-frontend/src/pages/Login/index.tsx
+++ todo-frontend/src/pages/Login/index.tsx
@@ -1,8 +1,33 @@
+import "./index.css";
+
 function Login() {
   return (
-    <div>
-      <div className="flex center full-screen">
-        <h3>Login</h3>
+    <div className="full-screen flex center login-page">
+      <div className="background-primary login-form">
+        <div className="flex center column full-height">
+          <form className="flex column gap center full-width">
+            <div className="flex column gap">
+              <label htmlFor="email" className="color-white">
+                Email
+              </label>
+              <input type="email" id="email" name="email" className="input" />
+            </div>
+            <div className="flex column gap">
+              <label htmlFor="password" className="color-white">
+                Password
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="input"
+              />
+            </div>
+            <button type="submit" className="btn">
+              Login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
